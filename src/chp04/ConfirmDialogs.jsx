e4dc53e18d7e7bba4788d2c4bdc943f8314d27b9
idsdk <idsdkk@gmail.com>
2025-09-02 16:28:17 +0900

    Button, ConfirmDialog, ConfirmDialogs, Clock 함수형 컴퍼넌트(React Element Rendering)

---
+++ src/chp04/ConfirmDialogs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function ConfirmDialogs(props) {
+    return(
+        <div className='div-bg-crimson'>
+            <p>Hi People.</p>
+            <br></br>
+        </div>
+    )
+}
+
+export default ConfirmDialogs;
